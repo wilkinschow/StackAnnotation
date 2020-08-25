bb28035737d98eb83827608f520b9ef3a6f0c8de
Shao Liang, Wilkins Chow <wilkins.chow@ncs.com.sg>
2020-08-25 14:04:48 +0800

    fail the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,6 +31,7 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
+		/*
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
@@ -38,6 +39,10 @@ public class MyStringStackTest extends TestCase {
 		stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
+		*/
+		MyStringStack stack = new MyStringStack();
+		stack.push (s1);
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
