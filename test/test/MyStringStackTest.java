2517a114904908c6e7ef847e50010c7baf463123
Shao Liang, Wilkins Chow <wilkins.chow@ncs.com.sg>
2020-08-25 14:10:09 +0800

    Revert "fail the test case"
    This reverts commit bb28035737d98eb83827608f520b9ef3a6f0c8de.
    
    Reverting changes

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,7 +31,6 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		/*
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
@@ -39,10 +38,6 @@ public class MyStringStackTest extends TestCase {
 		stack.push (s1);
         stack.push (s2);
         assertFalse (stack.isEmpty());
-		*/
-		MyStringStack stack = new MyStringStack();
-		stack.push (s1);
-		assertTrue (stack.isEmpty());
 	}
 
 	@Test
